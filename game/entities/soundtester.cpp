d250a3df327c4b79b82265f33b92aac3678e8f91
barneslt63@gmail.com <logan_barnes@brown.edu>
2015-04-19 23:41:26 -0400

    removed warning from soundtester

--- game/entities/soundtester.cpp
+++ game/entities/soundtester.cpp
@@ -1,10 +1,8 @@
 #include "soundtester.h"
 
-SoundTester::SoundTester(Audio *audio, glm::vec3 pos)
+SoundTester::SoundTester(glm::vec3 pos)
     : MovableEntity(pos)
 {
-//    this->setSound(audio, "kickDrumMod.wav", true);
-//    this->playSound();
 }
 
 SoundTester::~SoundTester()
